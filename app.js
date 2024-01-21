af630372c400d3d35a7a89f941706ef701031b51
lomkar <lomkar808@gmail.com>
2024-01-21 20:01:03 +0530

    routes refactor"

--- app.js
+++ app.js
@@ -7,80 +7,24 @@ const app = express();
 const { PrismaClient } = require("@prisma/client");
 const { apiUrl, webappUrl } = require("./config");
 const prisma = new PrismaClient();
-
-app.use(bodyParser.json());
+const routes = require("./routes/index");
 
 app.use(morgan("dev"));
 
 const port = process.env.PORT || 3001;
 
 //My Routes
-const corsOptions = {
-  origin: 'https://jsonmatchweb.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+app.use(cors());
 
-app.use(cors(corsOptions));
-
-app.get("/api",(req,res)=>{
+app.get("/api", (req, res) => {
   return res.json({
-    success:true,
-    message:"API is working"
-  })
-})
-app.post("/api/public", async (req, res) => {
-  try {
-    const { jsonData } = req.body;
-    const result = await prisma.publicJsonData.create({
-      data: {
-        JsonData: jsonData,
-      },
-      select: {
-        id: true,
-        JsonData: true,
-      },
-    });
-
-    return res.status(201).json({
-      message: "User created Successfully",
-      success: true,
-      url: `${apiUrl}/api/public/${result.id}`,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      error: "Something went wrong",
-    });
-  }
+    success: true,
+    message: "API is working",
+  });
 });
 
-app.get("/api/public/:publicid", async (req, res) => {
-  try {
-    const publicid = req.params.publicid;
-    const result = await prisma.publicJsonData.findUnique({
-      where: {
-        id: publicid,
-      },
-      select: {
-        JsonData: true,
-      },
-    });
-
-    if (!result) {
-      return res.status(500).json({ error: "No json found." });
-    }
-
-    return res.status(200).json(result.JsonData);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      success: false,
-      error: "Something went wrong",
-    });
-  }
-});
+app.use("/api", routes);
+app.use(bodyParser.json());
 
 //catch 404 errors and forwared then to error handler
 app.use((req, res, next) => {
